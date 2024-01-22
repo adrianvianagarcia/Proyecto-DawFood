da9833f8dcf007881f1077577182c405ad082572
AdrianViana <adrianvianagarcia@gmail.com>
2024-01-22 23:20:39 +0100

    Constructor y atributos del ticket creado, falta copiar arraylist cesta

--- src/main/java/daw/Ticket.java
+++ src/main/java/daw/Ticket.java
@@ -1,15 +1,25 @@
 
 package daw;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Ticket {
     
     int id;
+    int numeroPedido;
+    double precioTotal;
+    LocalDateTime fechaPedido;
+    Catalogo c1;
 
-    public Ticket(int id) {
+    public Ticket(int id, int numeroPedido, double precioTotal, LocalDateTime fechaPedido, Catalogo c1) {
         this.id = id;
+        this.numeroPedido = numeroPedido;
+        this.precioTotal = precioTotal;
+        this.fechaPedido = fechaPedido;
+        this.c1 = 
     }
+    
     
     
 }
