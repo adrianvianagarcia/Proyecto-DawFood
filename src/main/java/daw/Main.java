518beca8daa849d1d5bdead7bbeb8acdef1fd355
Omorval3008 <omorval3008@g.educaand.es>
2024-01-23 12:52:39 +0100

    idea nuevo menu seleccion implementada en tpv (hace falta implementarla en los selewctores)

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         //Encendemos el tpv//
-        Tpv tpv =new Tpv("Estepona");
+        Tpv tpv =new Tpv("Estepona");///
+        
         tpv.encenderTPV();
       
     }
