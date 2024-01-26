981f004c8be70d50caefa86b1c4cb001d48b71ac
Omorval3008 <omorval3008@g.educaand.es>
2024-01-26 10:18:50 +0100

    Iniciada la pasarela de pago

--- src/main/java/daw/Metodos.java
+++ src/main/java/daw/Metodos.java
@@ -4,22 +4,38 @@
  */
 package daw;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
  *
  * @author Óscar
  */
-public class Metodos{
-    
+public class Metodos {
+
     public static String mostrarProducto(ArrayList<Productos> lista) {
         String aux = "";
         for (int i = 0; i < lista.size(); i++) {/*Actualiza mi String y le añade
             todas las palabras en cada iteracion*/
-            aux += ( lista.get(i).getDescripcion()
-                    +" "+lista.get(i).getPrecioBase()+("\n"));
+            aux += (lista.get(i).getDescripcion()
+                    + " " + lista.get(i).getPrecioBase() + ("\n"));
         }
         return aux;
     }
-    
+
+    public static boolean pasarelaDePago(ArrayList<TarjetasDeCredito> baseDatosTarjetas) {
+        boolean compraValida = false;
+        for (int i = 0; i < baseDatosTarjetas.size(); i++) {
+            if (baseDatosTarjetas.get(i).verificarNumeroTarjeta(baseDatosTarjetas, MenusDeOpciones.solicitarCVV()) == true) {
+                if (baseDatosTarjetas.get(i).verificarCVV(baseDatosTarjetas, 88) == true) {
+                    if (baseDatosTarjetas.get(i).verificarFechaCaducidad(baseDatosTarjetas,null)==true) {
+                        compraValida=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return compraValida;
+    }
+
 }
